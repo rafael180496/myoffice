758f03f05edea282c4bcfd284440fe0daac3ed44
rhidalgo <rafael180496@gmail.com>
2019-12-26 15:55:23 -0600

    cambios previuw

--- src/page/portafolio.js
+++ src/page/portafolio.js
@@ -1,15 +1,108 @@
-import React,{ Component } from "react";
+import React, { Component, Fragment } from "react";
 import { AppSer } from "../service/funciones";
+import { ConstApp } from "../service/constantes";
+
+const CardContent = props => {
+  const { card } = props;
+  const hrefid = `#${card.alt}`;
+  return (
+    <div className="col-md-4 col-sm-6 portfolio-item">
+      <a className="portfolio-link" data-toggle="modal" href={hrefid}>
+        <div className="portfolio-hover">
+          <div className="portfolio-hover-content">
+            <i className="fas fa-plus fa-3x"></i>
+          </div>
+        </div>
+        <img className="img-fluid" src={card.img} alt={card.alt}></img>
+      </a>
+      <div className="portfolio-caption">
+        <h4>{card.titulo}</h4>
+      </div>
+    </div>
+  );
+};
+const CardModal = props => {
+  const { card } = props;
+  const hrefid = `${card.alt}`;
+  return (
+    <div
+      className="portfolio-modal modal fade"
+      id={hrefid}
+      tabIndex="-1"
+      role="dialog"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="close-modal" data-dismiss="modal">
+            <div className="lr">
+              <div className="rl"></div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 mx-auto">
+                <h2 className="text-uppercase">{card.titulo}</h2>
+
+                <img className="img-fluid" src={card.img} alt={card.alt}></img>
+                <p>
+                  {card.descripcion}
+                </p>
+                <button
+                  className="btn btn-primary"
+                  data-dismiss="modal"
+                  type="button"
+                >
+                  <i className="fas fa-times"></i>
+                  Cerrar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 class PortPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    componentDidMount() {
-        AppSer.NavCamb(false);
-    }
-    render() { 
-       return ( <p> hola </p>)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    AppSer.NavCamb(false);
+  }
+  render() {
+    return (
+      <Fragment>
+        <section className="bg-light page-section" id="portfolio">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <h2 className="section-heading text-uppercase">
+                  Muesta de Minerales
+                </h2>
+                <h3 className="section-subheading text-muted">
+                  Estos son algunos de nuestro minerales extraido sin procesar.
+                </h3>
+              </div>
+            </div>
+            <div className="row">
+              {ConstApp.Muestra.map((item, i) => {
+                return (
+                  <CardContent card={item} key={i + "idport"}></CardContent>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+        {ConstApp.Muestra.map((item, i) => {
+                return (
+                  <CardModal card={item} key={i + "idmodal"}></CardModal>
+                );
+              })}
+      </Fragment>
+    );
+  }
 }
 export default PortPage;
--- src/service/constantes.js
+++ src/service/constantes.js
@@ -1,38 +1,74 @@
-import diamond from '../img/diamond.png'
-import ruby from '../img/ruby.png'
-import gemstone from '../img/gemstone.png'
+import diamond from "../img/diamond.png";
+import ruby from "../img/ruby.png";
+import gemstone from "../img/gemstone.png";
 
+const Muestra = [
+  {
+    titulo: "Turmalina",
+    descripcion:
+      "El nombre turmalina procede de la palabra cingalesa «touramalli», que significa, «piedras de colores mezclados» y esta denominación fue originalmente aplicada a una serie de piedras, principalmente circones. ",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/minerales-nicaragua.appspot.com/o/imgs%2Fpiedras%2FPiedra1.jpg?alt=media&token=71bea61a-45d1-47cb-a826-cd20d52d7ee1",
+    alt: "piedra1"
+  },{
+    titulo: "Turmalina",
+    descripcion:
+      "El nombre turmalina procede de la palabra cingalesa «touramalli», que significa, «piedras de colores mezclados» y esta denominación fue originalmente aplicada a una serie de piedras, principalmente circones. ",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/minerales-nicaragua.appspot.com/o/imgs%2Fpiedras%2FPiedra2.jpg?alt=media&token=e7c8c211-843b-46f3-bee9-1617989a596d",
+    alt: "piedra2"
+  },{
+    titulo: "Ámbar",
+    descripcion:
+      "El ámbar, árabe o succino (del latín succinum) es resina fosilizada de origen vegetal, proveniente principalmente de restos de coníferas y algunas angiospermas. Etimológicamente su nombre proviene del árabe عنبر, ámbar, significando , ya que este flota sobre el agua del mar, aunque originalmente se refería al ámbar gris. Es de color marrón claro normalmente, aunque existen variedades amarillas, tono miel y verdosas. Está considerada como piedra semipreciosa. Ha sido apreciada por su color y belleza natural desde los tiempos del Neolítico.1​ El ámbar es utilizado en joyería y como agente curativo en la medicina popular",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/minerales-nicaragua.appspot.com/o/imgs%2Fpiedras%2FPiedra3.jpg?alt=media&token=464f1651-5bd7-4238-b93d-a07576883cef",
+    alt: "piedra3"
+  },{
+    titulo: "Cuarzo",
+    descripcion:
+      "El cuarzo es un mineral compuesto de sílice. Tras el feldespato es el mineral más común de la corteza terrestre estando presente en una gran cantidad de rocas ígneas, metamórficas y sedimentarias. Destaca por su dureza y resistencia a la meteorización en la superficie terrestre.",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/minerales-nicaragua.appspot.com/o/imgs%2Fpiedras%2FPiedra4.jpg?alt=media&token=beeb23e7-c87d-4a1e-bacd-f21d9a7586f2",
+    alt: "piedra4"
+  }
+];
 
-const Contacto={
-    Telefono:"+50588205311"
-}
+const Contacto = {
+  Telefono: "+50588205311"
+};
 const AppName = "MineralNic";
-const AppNameLength ="Minerales Nicaragua"
-const Servicios =[
-    {
-        titulo:"Ventas de minerales",
-        Descripcion:"Como empresa le vendemos variadad de minerales preciosos extraido directamente de minas locales.Por mayor y unidad",
-        Img:diamond,
-        alt:"diamond"
-    },    {
-        titulo:"Estudio y muestreo",
-        Descripcion:"Analizamos cualquier zona de potencial para extraccion de minerales certificando e haciendo un estudio si es factible dicha extraccion.",
-        Img:ruby,
-        alt:"ruby"
-    },    {
-        titulo:"Envio y Procesado",
-        Descripcion:"Enviamos cargas por mayor a todo Centro America y por unidad a nivel nacional.Tambien procesamos las piedras dejandola ya para hacerlas en joyeria.",
-        Img:gemstone,
-        alt:"gemstone"
-    },
-]
-
+const AppNameLength = "Minerales Nicaragua";
+const Servicios = [
+  {
+    titulo: "Ventas de minerales",
+    Descripcion:
+      "Como empresa le vendemos variadad de minerales preciosos extraido directamente de minas locales.Por mayor y unidad",
+    Img: diamond,
+    alt: "diamond"
+  },
+  {
+    titulo: "Estudio y muestreo",
+    Descripcion:
+      "Analizamos cualquier zona de potencial para extraccion de minerales certificando e haciendo un estudio si es factible dicha extraccion.",
+    Img: ruby,
+    alt: "ruby"
+  },
+  {
+    titulo: "Envio y Procesado",
+    Descripcion:
+      "Enviamos cargas por mayor a todo Centro America y por unidad a nivel nacional.Tambien procesamos las piedras dejandola ya para hacerlas en joyeria.",
+    Img: gemstone,
+    alt: "gemstone"
+  }
+];
 
 const ConstApp = {
   AppName,
   AppNameLength,
   Servicios,
-  Contacto
+  Contacto,
+  Muestra
 };
 
 export { ConstApp };
